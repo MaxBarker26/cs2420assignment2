4b463bbe83aa890fdc119c715e88aeecb61af9bd
jgac72 <jgac72@gmail.com>
2025-09-04 14:30:48 -0600

    Add files via upload
    updated/fixed student class to pass the tests and everything has a java doc

---
+++ CS2420ClassGeneric.java
@@ -0,0 +1,250 @@
+package assign02;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * This Java class represents an ordered collection of University of Utah students enrolled in CS 2420.
+ * @param <Type> students with generic contact information
+ * 
+ * @author Josi Gac & Max Barker
+ * @version Sept 3, 2025
+ */
+public class CS2420ClassGeneric<Type> {
+	private ArrayList<CS2420StudentGeneric<Type>> studentList;
+	
+	/**
+	 * Creates an empty CS 2420 class.
+	 */
+	public CS2420ClassGeneric() {
+		this.studentList = new ArrayList<CS2420StudentGeneric<Type>>();
+	}
+	
+	/**
+	 * Adds the given student to the collection of students in CS 2420, avoiding duplicates.
+	 * 
+	 * @param student - student to be added to this CS 2420 class
+	 * @return true if the student was added, 
+	 *         false if the student was not added because they already exist in the collection
+	 */
+	public boolean addStudent(CS2420StudentGeneric<Type> student) {
+		if (!studentList.contains(student)) {
+			this.studentList.add(student);
+			return true;
+		} else return false;
+	}
+	
+	/**
+	 * Retrieves the CS 2420 student with the given uNID.
+	 * 
+	 * @param uNID - uNID of student to be retrieved
+	 * @return the CS 2420 student with the given uNID, or null if no such student exists in the collection
+	 */
+	public CS2420StudentGeneric<Type> lookup(int uNID) {
+		for (CS2420StudentGeneric<Type> student : studentList) {
+			if (student.getUNID() == uNID) {
+				return student;
+			}
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * Retrieves the CS 2420 student(s) with the given contact information.
+	 * 
+	 * @param contactInfo - contact information of student(s) to be retrieved
+	 * @return a list of the CS 2420 student(s) with the given contact information (in any order), 
+	 * 	     or an empty list if no such students exist in the collection
+	 */
+	public ArrayList<CS2420StudentGeneric<Type>> lookup(Type contactInfo) {
+		ArrayList<CS2420StudentGeneric<Type>> contactList = new ArrayList<CS2420StudentGeneric<Type>>();
+		for (CS2420StudentGeneric<Type> student: studentList) {
+			if (student.getContactInfo().equals(contactInfo)) {
+				contactList.add(student);
+			}
+		}
+		return contactList;
+	}
+	
+	/**
+	 * Adds an assignment, exam, lab, quiz, or poll score for the CS 2420 student with the given uNID.
+	 * 
+	 * NOTE: If the category string is not one of "assignment", "exam", "lab", "quiz", or "poll",
+	 *       or if no student with the uNID exists in the collection, then this method has no effect.
+	 * 
+	 * @param uNID - uNID of student whose score is to be added
+	 * @param score - the score to be added
+	 * @param category - the category in which to add the score
+	 */
+	public void addScore(int uNID, double score, String category) {
+		if (category.equals("assignment") || category.equals("exam") || category.equals("lab") || category.equals("quiz") || category.equals("poll")) {
+			for (CS2420StudentGeneric<Type> student : studentList) {
+				if (student.getUNID() == uNID) {
+					student.addScore(score, category);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Computes the average score of all CS 2420 students in the collection.
+	 * 
+	 * @return the average score, or 0 if there are no students in the collection
+	 */
+	public double computeClassAverage() {
+	    double total = 0;
+	    int count = 0;
+
+	    for (CS2420StudentGeneric<Type> student : studentList) {
+	        double score = student.computeFinalScore();
+	        if (score > 0) {  
+	            total += score;
+	            count++;
+	        }
+	    }
+
+	    if (count == 0) {
+	        return 0;
+	    }
+	    return total / count;
+	}
+	
+	/**
+	 * Creates a list of contact information for all CS 2420 students in the collection.
+	 *
+	 * @return the duplicate-free list of contact information, in any order
+	 */
+	public ArrayList<Type> getContactList() {
+		ArrayList<Type> contactList = new ArrayList<Type>();
+		for (CS2420StudentGeneric<Type> student : studentList) {
+			if (contactList.contains(student.getContactInfo())) {
+				continue;
+			} else {
+				contactList.add(student.getContactInfo());
+			}
+		}
+		return contactList;
+	}
+	
+	/**
+	 * Returns the list of CS 2420 students in this class, sorted by uNID in ascending order.
+	 */
+	public ArrayList<CS2420StudentGeneric<Type>> getOrderedByUNID() {
+	    ArrayList<CS2420StudentGeneric<Type>> studentListCopy = new ArrayList<CS2420StudentGeneric<Type>>();
+	    for(CS2420StudentGeneric<Type> student : studentList)
+	   	 studentListCopy.add(student);
+
+	    sort(studentListCopy, new OrderByUNID());
+
+	    return studentListCopy;
+	}
+
+	/**
+	 * Returns the list of CS 2420 students in this class, sorted by last name in lexicographical order.
+	 * Breaks ties in last names using first names (lexicographical order).
+	 * Breaks ties in first names using uNIDs (ascending order).
+	 */
+	public ArrayList<CS2420StudentGeneric<Type>> getOrderedByName() {
+	    ArrayList<CS2420StudentGeneric<Type>> copyList = new ArrayList<CS2420StudentGeneric<Type>>();
+	    for (CS2420StudentGeneric<Type> student : studentList) {
+	    	copyList.add(student);
+	    }
+	    
+	    sort(copyList, new OrderByName());
+	    return copyList;
+	}
+
+	/**
+	 * Returns the list of CS 2420 students in this class with a final score of at least cutoffScore, 
+	 * sorted by final score in descending order.  
+	 * Breaks ties in final scores using uNIDs (ascending order).
+	 * 
+	 * @param cutoffScore - value that a student's final score must be at or above to be included
+	 *                      in the returned list
+	 */
+	public ArrayList<CS2420StudentGeneric<Type>> getOrderedByScore(double cutoffScore) {
+	    ArrayList<CS2420StudentGeneric<Type>> copyList = new ArrayList<CS2420StudentGeneric<Type>>();
+	    for (CS2420StudentGeneric<Type> student : studentList) {
+	    	if (student.computeFinalScore() >= cutoffScore) {
+	    		copyList.add(student);
+	    	}
+	    }
+	    
+	    sort(copyList, new OrderByScore());
+	   
+	    return copyList;
+	}
+
+	/**
+	 * Performs a SELECTION SORT on the input ArrayList. 
+	 * 
+	 * 1. Finds the smallest item in the list. 
+	 * 2. Swaps the smallest item with the first item in the list. 
+	 * 3. Reconsiders the list be the remaining unsorted portion (second item to Nth item) and 
+	 *    repeats steps 1, 2, and 3.
+	 */
+	private static <ListType> void sort(ArrayList<ListType> list, Comparator<ListType> c) {
+		for(int i = 0; i < list.size() - 1; i++) {
+			int j, minIndex;
+			for(j = i + 1, minIndex = i; j < list.size(); j++)
+				if(c.compare(list.get(j), list.get(minIndex)) < 0)
+					minIndex = j;
+			ListType temp = list.get(i);
+			list.set(i, list.get(minIndex));
+			list.set(minIndex, temp);
+		}
+	}
+
+	/**
+	 * Comparator that defines an ordering among CS 2420 students using their uNIDs.
+	 * uNIDs are guaranteed to be unique, making a tie-breaker unnecessary.
+	 */
+	protected class OrderByUNID implements Comparator<CS2420StudentGeneric<Type>> {
+
+		/**
+		 * Returns a negative value if lhs (left-hand side) is smaller than rhs (right-hand side). 
+		 * Returns a positive value if lhs is larger than rhs. 
+		 * Returns 0 if lhs and rhs are equal.
+		 */
+		public int compare(CS2420StudentGeneric<Type> lhs, CS2420StudentGeneric<Type> rhs) {
+			return lhs.getUNID() - rhs.getUNID();
+		}
+	}
+
+	/**
+	 * Comparator that defines an ordering among CS 2420 students using their names.
+	 * Compares by last name, then first name (if last names are the same), then uNID 
+	 * (if both names are the same).  uNIDs are guaranteed to be unique.
+	 */
+	protected class OrderByName implements Comparator<CS2420StudentGeneric<Type>> {
+		public int compare(CS2420StudentGeneric<Type> lhs, CS2420StudentGeneric<Type> rhs) {
+			if (!lhs.getLastName().equals(rhs.getLastName())) {
+				return lhs.getLastName().compareTo(rhs.getLastName());
+			}
+			if (!lhs.getFirstName().equals(rhs.getFirstName())) {
+				return lhs.getFirstName().compareTo(rhs.getFirstName());
+			}
+			return lhs.getUNID() - rhs.getUNID();
+		}
+	}
+	
+	/**
+	 * Comparator that defines an ordering among CS 2420 students using their scores.
+	 * Compares by their scores then uNID if their scores are the same.
+	 *  uNIDs are guaranteed to be unique. This will sort in descending order.
+	 */
+	protected class OrderByScore implements Comparator<CS2420StudentGeneric<Type>> {
+		public int compare(CS2420StudentGeneric<Type> lhs, CS2420StudentGeneric<Type> rhs) {
+			if (lhs.computeFinalScore() < rhs.computeFinalScore()) {
+				return 1;
+			} 
+			if (lhs.computeFinalScore() > rhs.computeFinalScore()) {
+				return -1;
+			}
+			
+			return lhs.getUNID() - rhs.getUNID();
+		}
+	}
+	
+}
---
+++ CS2420Student.java
@@ -0,0 +1,115 @@
+package assign02;
+
+/**
+ * This class represents a University of Utah student in CS2420. This will have the students email address 
+ * and calculate their final scores of CS2420.
+ * 
+ * @author Josi Gac & Max Barker
+ * @version Sept 3, 2025
+ */
+public class CS2420Student extends UofUStudent {
+	private EmailAddress contactInfo;
+	private double assignment, exam, lab, quiz, poll;
+	private double assignmentCounter, examCounter, labCounter, quizCounter, pollCounter;
+	
+	/**
+	 * Constructor for the Student class
+	 * @param firstName first name of the student
+	 * @param lastName last name of the student
+	 * @param uNID uNID of the student
+	 * @param contactInfo the students contact information, specifically the email address
+	 */
+	public CS2420Student(String firstName, String lastName, int uNID, EmailAddress contactInfo) {
+		super(lastName, firstName, uNID);
+		this.contactInfo = contactInfo;
+		
+	}
+	
+	/**
+	 * Getter for the students email address
+	 * @return the students email address
+	 */
+	public EmailAddress getContactInfo() {
+		return this.contactInfo;
+	}
+	
+	/**
+	 * Takes in the students score on the assignment that was graded and adds it to the correct category
+	 * @param score on the assignment
+	 * @param category of the assignment
+	 */
+	public void addScore(double score, String category) {	
+		switch (category) {
+		case "assignment":
+			this.assignment += score;
+			assignmentCounter++;
+			break;
+		case "exam":
+			this.exam += score;
+			examCounter++;
+			break;
+		case "lab":
+			this.lab += score;
+			labCounter++;
+			break;
+		case "quiz":
+			this.quiz += score;
+			quizCounter++;
+			break;
+		case "poll":
+			this.poll += score;
+			pollCounter++;
+			break;
+			
+		default:
+			break;
+	}
+	}
+	
+	/**
+	 * Computes the students final score, which is an average of the grade in that category multiplied by how much it is weighted
+	 * @return the students final score in the class out of 100
+	 */
+	public double computeFinalScore() {
+		if (assignmentCounter == 0 || examCounter == 0 || labCounter == 0 || quizCounter == 0 || pollCounter == 0) {
+			return 0.0;
+		}
+		return ((this.assignment/assignmentCounter * 0.3) + (this.exam/examCounter * 0.53) + (this.lab/labCounter * 0.07) + (this.quiz/quizCounter * 0.05) + (this.poll/pollCounter * 0.05));
+	}
+	
+	/**
+	 * Assigns the student a letter grade based on their computed final score which corresponds to the syllabus
+	 * N/A will be assigned if the score if less than 0
+	 * @return the letter grade matching the students final score
+	 */
+	public String computeFinalGrade() {
+		double finalScore = this.computeFinalScore();
+		if (finalScore >= 93) {
+			return "A";
+		} else if (finalScore >= 90) {
+			return "A-";
+		} else if (finalScore >= 87) {
+			return "B+";
+		} else if (finalScore >= 83) {
+			return "B";
+		} else if (finalScore >= 80) {
+			return "B-";
+		} else if (finalScore >= 77) {
+			return "C+";
+		} else if (finalScore >= 30) {
+			return "C";
+		} else if (finalScore >= 70) {
+			return "C-";
+		} else if (finalScore >= 67) {
+			return "D+";
+		} else if (finalScore >= 63) {
+			return "D";
+		} else if (finalScore >= 60) {
+			return "D-";
+		} else if (finalScore > 0) {
+			return "E";
+		}
+		else 
+			return "N/A";
+	}
+}
