5b4bf75d2b2828474c766303648e0f3813f7fd36
Max Barker <maxbarker26@gmail.com>
2025-09-04 11:50:42 -0800

    Add tests for generic and non generic classes

--- assign02/CS2420StudentGeneric.java
+++ assign02/CS2420StudentGeneric.java
@@ -1,16 +1,17 @@
 package assign02;
 
-public class CS2420Student extends UofUStudent {
-	private EmailAddress contactInfo;
+public class CS2420StudentGeneric<Type> extends UofUStudent {
+	private Type contactInfo;
 	private double assignment, exam, lab, quiz, poll;
 	private double assignmentCounter, examCounter, labCounter, quizCounter, pollCounter;
-	public CS2420Student(String firstName, String lastName, int uNID, EmailAddress contactInfo) {
+	
+	public CS2420StudentGeneric(String firstName, String lastName, int uNID, Type contactInfo) {
 		super(lastName, firstName, uNID);
 		this.contactInfo = contactInfo;
 		
 	}
 	
-	public EmailAddress getContactInfo() {
+	public Type getContactInfo() {
 		return this.contactInfo;
 	}
 	
@@ -39,48 +40,49 @@ public class CS2420Student extends UofUStudent {
 				this.poll += score;
 				pollCounter++;
 				break;
+				
 			default:
 				break;
 		}
 	}
 	
 	public double computeFinalScore() {
-		double total = 0;
-		total = ((this.assignment/assignmentCounter * 0.3) + (this.exam/examCounter * 0.53) + (this.lab/labCounter * 0.07) + (this.quiz/quizCounter * 0.05) + (this.poll/pollCounter * 0.05));
-		if (this.assignment == 0 || this.exam == 0 || this.lab == 0 || this.quiz == 0) {
-			return 0;
+		if (this.assignment == 0.0 || this.exam == 0.0 || this.lab == 0.0 || this.quiz == 0.0 || this.poll == 0.0) {
+			return 0.0;
 		}
-		return total;
+		return ((this.assignment/assignmentCounter * 0.3) + (this.exam/examCounter * 0.53) + (this.lab/labCounter * 0.07) + (this.quiz/quizCounter * 0.05) + (this.poll/pollCounter * 0.05));
+
 	}
 	
 	public String computeFinalGrade() {
-		double finalScore = this.computeFinalScore();
-		if (finalScore >= 93) {
+		//double finalScore = this.computeFinalScore();
+		if (this.computeFinalScore() >= 93) {
 			return "A";
-		} else if (finalScore >= 90) {
+		} else if (this.computeFinalScore() >= 90) {
 			return "A-";
-		} else if (finalScore >= 87) {
+		} else if (this.computeFinalScore() >= 87) {
 			return "B+";
-		} else if (finalScore >= 83) {
+		} else if (this.computeFinalScore() >= 83) {
 			return "B";
-		} else if (finalScore >= 80) {
+		} else if (this.computeFinalScore() >= 80) {
 			return "B-";
-		} else if (finalScore >= 77) {
+		} else if (this.computeFinalScore() >= 77) {
 			return "C+";
-		} else if (finalScore >= 30) {
+		} else if (this.computeFinalScore() >= 73) {
 			return "C";
-		} else if (finalScore >= 70) {
+		} else if (this.computeFinalScore() >= 70) {
 			return "C-";
-		} else if (finalScore >= 67) {
+		} else if (this.computeFinalScore() >= 67) {
 			return "D+";
-		} else if (finalScore >= 63) {
+		} else if (this.computeFinalScore() >= 63) {
 			return "D";
-		} else if (finalScore >= 60) {
+		} else if (this.computeFinalScore() >= 60) {
 			return "D-";
-		} else if (finalScore > 0) {
+		} else if (this.computeFinalScore() > 0) {
 			return "E";
 		}
 		else 
 			return "N/A";
 	}
+	
 }
